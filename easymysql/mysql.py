12ebd795e93a7a6ed36d7fc8c6d92145487cb632
Alvaro <9125409+alvarodeleon@users.noreply.github.com>
2024-12-29 20:29:16 -0300

    Update 0.1.9.1

--- easymysql/mysql.py
+++ easymysql/mysql.py
@@ -2,34 +2,99 @@
 # -*- coding: utf-8 -*-
 
 import pymysql
-import pymysql.cursors
-
 import collections
 
 class mysql:
 
-	__cursor = None
-	__db = None
-	__result = None
+		__cursor = None
+		__db = None
+		__result = None
 
-	def __init__(self,hostname,username,password,database):
+		__hostname = ""
+		__username = ""
+		__password = ""
+		__database = ""
 
-		self.__db = pymysql.connect(host=hostname,user=username,password=password,database=database)
+		def __init__(self,hostname,username,password,database):
 
-		self.__cursor = self.__db.cursor()
+				self.__hostname = hostname
+				self.__username = username
+				self.__password = password
+				self.__database = database
 
-	def execute(self,query):
+				self.connect()
 
-		try:
-			self.__cursor.execute(query)
+		def version(self):
+				return "0.1.9.1"
 
-		except Exception as e:
-			print("Exeception occured:{}".format(e))
+		def ping(self):
+				if self.__db.ping(reconnect=True):
+						return True
+				else:
+						return False
 
-	def query(self,query):
+		def connect(self):
+				self.__db = pymysql.connect(host=self.__hostname, user=self.__username, password=self.__password, database=self.__database)
 
-		try:
-			self.__cursor.execute(query)
+				self.__cursor = self.__db.cursor()
+
+		def reconnect(self):
+				self.connect()
+
+		def execute(self,query):
+
+				if self.ping() == False:
+						self.reconnect()
+
+				try:
+						self.__cursor.execute(query)
+
+				except Exception as e:
+						print("Exeception occured:{}".format(e))
+
+		def query(self,query):
+
+				if self.ping() == False:
+						self.reconnect()
+
+				try:
+						self.__cursor.execute(query)
+
+						columns = map(lambda x:x[0], self.__cursor.description)
+						result = [dict(zip(	 columns,row))   for row in self.__cursor.fetchall()]
+
+						return result
+
+				except Exception as e:
+						print("Exeception occured:{}".format(e))
+
+		def count(self):
+				return self.__cursor.rowcount
+
+		def getLastId(self):
+				return self.__cursor.lastrowid
+
+		def select(self,table,condition="",fields="*",order=""):
+
+			if not isinstance(condition,str) and isinstance(condition,collections.Iterable):
+				tmp=""
+				for key in condition:
+					tmp = tmp + str(key) + "=\'" + str(condition[key]) + "\' AND "
+
+				if tmp.endswith("AND "):
+					tmp = tmp[0:-4]
+
+				condition = tmp
+
+			if len(condition) > 0:
+				query = "SELECT " + fields + " FROM " + table + " WHERE " + condition
+			else:
+				query = "SELECT " + fields + " FROM " + table
+
+			if len(order)>0:
+				query = query + " " + order
+
+			self.execute(query)
 
 			tmp = map(lambda x:x[0], self.__cursor.description)
 
@@ -37,125 +102,84 @@ class mysql:
 
 			for column in tmp:
 				columns.append(column)
-				
+
 			result = [dict(zip(	columns,row))   for row in self.__cursor.fetchall()]
 
 			return result
 
-		except Exception as e:
-			print("Exeception occured:{}".format(e))
+		def insert(self,table,data):
 
-	def count(self):
-		return self.__cursor.rowcount
+				fields = ''
+				values = '' 
+				for key in data:
+						fields = fields + key + ","
+						values = str(values) + "\'" + str(data[key]) + "\',"
 
-	def getLastId(self):
-		return self.__cursor.lastrowid
+				if fields.endswith(","):
+						fields = fields[0:-1]
 
-	def select(self,table,condition="",fields="*",order=""):
+				if values.endswith(","):
+						values = values[0:-1]
 
-		if not isinstance(condition,str) and isinstance(condition,collections.Iterable):
-			tmp=""
-			for key in condition:
-				tmp = tmp + str(key) + "=\'" + str(condition[key]) + "\' AND "
+				query = "INSERT INTO " + table + " (" + fields + ") VALUES (" + values + ");"
 
-			if tmp.endswith("AND "):
-				tmp = tmp[0:-4]
+				self.execute(query)
+				self.__db.commit()
 
-			condition = tmp
+				return self.__cursor.lastrowid
 
-		if len(condition) > 0:
-			query = "SELECT " + fields + " FROM " + table + " WHERE " + condition
-		else:
-			query = "SELECT " + fields + " FROM " + table
+		def update(self,table,data,condition):
 
-		if len(order)>0:
-			query = query + " " + order
+				values=''
+				where=''
 
-		self.execute(query)
+				if not isinstance(data,str) and isinstance(data,collections.Iterable):
+						for key in data:
+								values = str(values) + str(key) + "='" + str(data[key]) + "', "
 
-		tmp = map(lambda x:x[0], self.__cursor.description)
+						if values.endswith(", "):
+								values = values[0:-2]
 
-		columns = []
+				if not isinstance(condition,str) and isinstance(condition,collections.Iterable):
+						for key in condition:
+								where = str(where) + str(key) + "='" + str(condition[key]) + "' AND "
 
-		for column in tmp:
-			columns.append(column)
+						if where.endswith(" "):
+								where = where[0:-5]
+				else:
+						where = condition
 
-		result = [dict(zip(	columns,row))   for row in self.__cursor.fetchall()]
+				query = "UPDATE " + table + " SET " + values + " WHERE " + where + ";"
 
-		return result
-
-	def insert(self,table,data):
-
-		fields = ''
-		values = ''
-		for key in data:
-			fields = fields + key + ","
-			values = str(values) + "\'" + str(data[key]) + "\',"
-
-		if fields.endswith(","):
-			fields = fields[0:-1]
-
-		if values.endswith(","):
-			values = values[0:-1]
-
-		query = "INSERT INTO " + table + " (" + fields + ") VALUES (" + values + ");"
-
-		self.execute(query)
-		self.__db.commit()
-
-		return self.__cursor.lastrowid
-
-	def update(self,table,data,condition):
-
-		values=''
-		where=''
-
-		if not isinstance(data,str) and isinstance(data,collections.Iterable):
-			for key in data:
-				values = str(values) + str(key) + "='" + str(data[key]) + "', "
-
-			if values.endswith(", "):
-				values = values[0:-2]
-
-		if not isinstance(condition,str) and isinstance(condition,collections.Iterable):
-			for key in condition:
-				where = str(where) + str(key) + "='" + str(condition[key]) + "' AND "
-
-			if where.endswith(" "):
-				where = where[0:-5]
-		else:
-			where = condition
-
-		query = "UPDATE " + table + " SET " + values + " WHERE " + where + ";"
-
-		self.execute(query)
-		self.__db.commit()
+				self.execute(query)
+				self.__db.commit()
 
 
 
-	def delete(self,table,condition):
 
-		if not isinstance(condition,str) and isinstance(condition,collections.Iterable):
-			tmp=""
-			for key in condition:
-				tmp = tmp + str(key) + "=\'" + str(condition[key]) + "\' AND "
+		def delete(self,table,condition):
 
-			if tmp.endswith("AND "):
-				tmp = tmp[0:-4]
+				if not isinstance(condition,str) and isinstance(condition,collections.Iterable):
+						tmp=""
+						for key in condition:
+								tmp = tmp + str(key) + "=\'" + str(condition[key]) + "\' AND " 
 
-			condition = tmp
+						if tmp.endswith("AND "):
+								tmp = tmp[0:-4]
 
-		query = "DELETE FROM " + table + " WHERE " + condition + ";"
+						condition = tmp
 
-		self.execute(query)
-		self.__db.commit()
+				query = "DELETE FROM " + table + " WHERE " + condition + ";"
 
-	def close(self):
-		self.__db.close()
+				self.execute(query)
+				self.__db.commit()
 
-	def resetCache(self):
-		self.execute("RESET QUERY CACHE;")
+		def close(self):
+				self.__db.close()
 
-	def truncate(self,table):
-		if len(table)>0:
-			self.execute("TRUNCATE {};".format(table))
+		def resetCache(self):
+			self.__db.execute("RESET QUERY CACHE;")
+
+		def truncate(self,table):
+			if len(table)>0:
+				self.__db.execute("TRUNCATE {};".format(table))
